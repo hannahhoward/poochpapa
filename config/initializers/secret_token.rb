79fc6f5beecfc68ade099817e3881b3443351a5d
Hannah Howard <hannah@hannahhoward.net>
2013-11-22 22:35:42 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poochpapa::Application.config.secret_key_base = '31b79aee28f690450da8c04233471bfa246c33b7334c6723ef3917cabe755c4840f7b9ceca4594204df70b71d80f4e108d65364e2e892727c18260f1ba594d08'
