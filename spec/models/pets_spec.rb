2d085cf97ccc25cb2395c1ce95337c3490f9c5f3
Hannah Howard <hannah@hannahhoward.net>
2013-12-23 17:27:57 -0800

    Add client table

--- spec/models/pets_spec.rb
+++ spec/models/pets_spec.rb
@@ -8,4 +8,5 @@ describe Pet do
   it { should have_db_column :medical_history }
   it { should have_db_column :special_needs }
   it { should have_db_column :instructions }
+  it { should belong_to :client }
 end
